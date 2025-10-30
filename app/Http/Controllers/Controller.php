5f7ae11799fcaea79d7e9c8725904222eb88981c
Azizah <90896327+sukasukajija@users.noreply.github.com>
2025-10-30 16:53:38 +0700

    cek

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,40 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Buku;
-use App\Models\Penerbit;
-use Illuminate\Http\Request;
 
 abstract class Controller
 {
-    public function index(Request $request)
-    {
-        $search = $request->query('cari');
 
-        $bukus = Buku::with('penerbit')
-            ->when($search, function ($query) use ($search) {
-                return $query->where('nama_buku', 'LIKE', '%' . $search . '%');
-            })
-            ->get();
-
-        return view('index', compact('bukus', 'search'));
-    }
-
-    public function admin()
-    {
-        $bukus = Buku::with('penerbit')->get();
-        $penerbits = Penerbit::all();
-        return view('admin', compact('bukus', 'penerbits'));
-    }
-
-    public function pengadaan()
-    {
-        $stok_limit = 20;
-        $bukus = Buku::with('penerbit')
-            ->where('stok', '<=', $stok_limit)
-            ->orderBy('stok', 'asc')
-            ->get();
-
-        return view('pengadaan', compact('bukus', 'stok_limit'));
-    }
 }
